c408ec68475ff79c196da97927860b1e3fb24e96
stela88 <stela.gal88@gmail.com>
2024-04-16 13:45:15 +0200

    Reading data from second sensor, connecting first sensor with camera

--- Sensor.py
+++ Sensor.py
@@ -14,38 +14,69 @@ GPIO.setmode(GPIO.BCM)
 pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 app = FastAPI()
 
-TRIG = 23
-ECHO = 24
+TRIG1 = 23
+ECHO1 = 24
+TRIG2 = 21
+ECHO2 = 20
 
-GPIO.setup(TRIG, GPIO.OUT)
-GPIO.setup(ECHO, GPIO.IN)
+GPIO.setup(TRIG1, GPIO.OUT)
+GPIO.setup(ECHO1, GPIO.IN)
+GPIO.setup(ECHO2, GPIO.IN)
+GPIO.setup(TRIG2, GPIO.OUT)
 
-GPIO.output(TRIG, False)
-print("Waiting For Sensor To Settle")
+GPIO.output(TRIG1, False)
+print("Waiting For Sensor 1 To Settle")
+time.sleep(2)
+GPIO.output(TRIG2, False)
+print("Waiting For Sensor 2 To Settle")
 time.sleep(2)
 
 registration_list = []
 
 try:
     while True:
-        GPIO.output(TRIG, True)
-        time.sleep(0.00001)
-        GPIO.output(TRIG, False)
 
-        while GPIO.input(ECHO) == 0:
+        # first sensor
+
+        GPIO.output(TRIG1, True)
+        time.sleep(0.1)
+        GPIO.output(TRIG1, False)
+
+        while GPIO.input(ECHO1) == 0:
             pulse_start = time.time()
 
-        while GPIO.input(ECHO) == 1:
+        while GPIO.input(ECHO1) == 1:
             pulse_end = time.time()
 
-        pulse_duration = pulse_end - pulse_start
+        pulse_duration_first = pulse_end - pulse_start
 
-        distance = pulse_duration * 17150
-        distance = round(distance, 2)
+        distance_first = pulse_duration_first * 17150
+        distance_first_sensor = round(distance_first, 2)
 
-        print("Distance:", distance, "cm")
+        print("Distance sensor 1:", distance_first_sensor, "cm")
 
-        if distance <= 8:
+        # second sensor
+
+        GPIO.output(TRIG2, True)
+        time.sleep(0.1)
+        GPIO.output(TRIG2, False)
+
+        while GPIO.input(ECHO2) == 0:
+            pulse_start = time.time()
+
+        while GPIO.input(ECHO2) == 1:
+            pulse_end = time.time()
+
+        pulse_duration_second = pulse_end - pulse_start
+
+        distance_second = pulse_duration_second * 17150
+        distance_second_sensor = round(distance_second, 2)
+
+        print("Distance sensor 2:", distance_second_sensor, "cm")
+
+        # camera
+
+        if distance_first_sensor <= 8:
             save_dir = '/home/sgal/images/pics/'
             print("Distance is 8cm or less. Taking pictures...")
 
@@ -96,7 +127,7 @@ try:
                 return max(set(registration_list), key=registration_list.count)
 
 
-            # Sending plate number
+            # Sending plate number to another endpoint
             response = requests.post(
                 'http://172.16.1.38:8082/tickets',
                 json={'registration': most_common_registration(registration_list)}
@@ -106,4 +137,7 @@ try:
 except KeyboardInterrupt:
     print("Program terminated by user.")
     GPIO.cleanup()
+
+
+
 
